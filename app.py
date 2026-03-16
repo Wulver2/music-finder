58bd017f56a6604751f8474aec3e6371c73abda2
Wulver2 <ross.shayla21@gmail.com>
2026-03-16 10:34:09 -0400

    Renders template from user's form input after user enters their artist, new page will be rendered that displays the tags for that artist. Later this page will display the info of other similar artists

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import requests
 import os
 
 app = Flask(__name__)
+# TODO: Format data, implement recommendation system, try autocomplete system,
+# add a way to go back to homepage after artist post
 
 #load_dotenv()
 email = os.getenv("MB_EMAIL")
@@ -18,21 +20,38 @@ api_url = "https://musicbrainz.org/ws/2/"
 def home():
     return render_template("index.html")
 
+@app.route('/', methods=["POST"])
+def info():
+    artist_name = request.form.get("userArtist")
+    print(artist_name)
+    info = get_artists_info(artist_name)
+    tags = info["artists"][0]["tags"]
+
+    return render_template("artists.html", info= tags)
+
+# TODO: Will allow retrival of other artists that match initial artist entered
+# 
+def make_recommendation(artist_info):
+    pass
+
 def get_artists_info(artist):
+
+
     #url = f"{api_url}?query=artist:{artist}?inc=aliases&fmt=json"
     url = f"{api_url}artist/?query=artist:{artist}&fmt=json"
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        print(f"data retrieved:{data["artists"][0]["tags"]}")
+        print(f"data retrieved")
+        return data
     elif response.status_code == 403:
         print("need a user-agent")
     else:
         print("error")
 
-artist_name = "Vince_Staples"
-get_artists_info(artist_name)
+# artist_name = "Vince_Staples"
+# get_artists_info(artist_name)
 
 
 if __name__ == '__main__n':
